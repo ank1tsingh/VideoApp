c92b8c102664d2866f6ee6c4444cc9fb515d40d1
Ankit <118796622+ank1tsingh@users.noreply.github.com>
2025-07-20 13:13:26 +0530

    Initial Commit

---
+++ VideoApp/VideoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoAppApp.swift
+//  VideoApp
+//
+//  Created by Ankit on 20/07/25.
+//
+
+import SwiftUI
+
+@main
+struct VideoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
